d3957080c6a8af648fb4e3e16d6e243000072775
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2023-02-05 05:33:26 +0000

    Fix tempfile & make url accessible in URLReader

--- src/gpkgstatus/URLReader.py
+++ src/gpkgstatus/URLReader.py
@@ -1,37 +1,45 @@
 import json
 import requests
-import tempfile
 
 from os.path import join
 from sys import exit
+from tempfile import gettempdir
 from termcolor import colored
 
 
 class URLReader:
     class _StatusException(Exception):
         pass
-    
-    def __init__(self, url: str):
+
+    _url: str = ""
+
+    def __init__(self, _url: str):
         try:
-            response = requests.head(url)
+            response = requests.head(_url)
             
             if response.status_code != 200:
                 print(colored("Error: Website isn't returning HTTP Status Code (200 OK)"))
                 raise URLReader._StatusException()
+            
+            self._url = _url
 
         except requests.ConnectionError:
-            print(colored(f"Error: Could not connect to {url}", "red"))
+            print(colored(f"Error: Could not connect to {_url}", "red"))
             exit(1)
     
     def __load_json(self):
-        response = requests.get(self.url)
-        return response.json()
+        try:
+            response = requests.get(self._url)
+            return response.json()
+        except:
+            print(colored("Error: There is an issue parsing JSON from corresponding website. Check if given URL is website JSON URL", "red"))
+            exit(1)
 
     def save_as_file(self, filename: str, data: str=None):
         if data is None:
             data = self.__load_json()
         
-        temp_file = join(tempfile.gettempdir(), filename)
+        temp_file = join(gettempdir(), filename)
         
         try:
             with open(temp_file, 'w') as file:
