6ec9ef805cb1fb3b3c398699bc15dc0f7c225c6a
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2023-02-05 06:16:18 +0000

    Add JSONFileReader
    This class seperates the file reading functionality of the program.
    It currently consists of following features:
    1. Given a path, it checks if it exists and is a file.
    2. If file name is only given instead of path, it adds tempdir
    before file name.
    3. It reads the file and returns appropriate keys.
    4. A limit is given for the output of read function.
    
    Warning: This class is untested and may not work as intended.

---
+++ src/gpkgstatus/JSONFileReader.py
@@ -0,0 +1,47 @@
+import os
+
+from json import load as jsonload
+from pathlib import Path
+from sys import exit
+from tempfile import gettempdir
+from termcolor import colored
+
+
+class JSONFileReader:
+    
+    _keys: list = []
+    _path: Path = Path()
+
+    def __init__(self, _path: Path | str, *_keys):
+        if len(_keys) < 1:
+            print(colored("Error: At least one key is required"))
+            exit(1)
+        
+        if not isinstance(_path):
+            _path = Path(os.path.join(gettempdir(), _path))
+        
+        if not (_path.exists() and _path.is_file()):
+            print(colored("Error: Given Path either doesn't exist or is not a file"))
+            exit(1)
+
+        self._keys = list(_keys)
+        self._path = _path
+    
+    def read(self, limit: int=5):
+        try:
+            with open(self._path, 'r') as file:
+                data = jsonload(file)
+                searched_info = []
+
+                for key in self._keys:
+                    if key not in data.keys:
+                        print(colored(f"Warning: Key {key} doesn't exist"))
+                    else:
+                        for value in data[key]:
+                            if len(searched_info) >= limit:
+                                return searched_info
+                            
+                            searched_info.append(value)
+        except PermissionError:
+            print(colored("Error: Permission denied. Please check temp directory permissions.", "red"))
+            exit(1)
