542cb96fa13184384778ed181e80a7e954d1ff12
Dhanush Kovi <99819848+dkvc@users.noreply.github.com>
2023-02-27 14:19:10 +0000

    fix: Use color for Command Prompt or Powershell in Windows 10
    Closes #40

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,12 @@
 # pylint:disable=missing-module-docstring
+import os
+
+from termcolor import colored
 from gpkgstatus.gpkgstatus import cli
 
-cli()
+if os.name == "nt":
+    os.system("color")
+    cli()
+    input(colored("Press Enter to continue...", "green"))
+else:
+    cli()
